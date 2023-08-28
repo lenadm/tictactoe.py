fff44bf2101479c7eec0dd73e5f9206adf6a2bb7
ben richards <lenarichards2234@gmail.com>
2023-08-28 18:50:12 +0100

    enemy functionality

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,4 @@
+import random
 
 board = [
     [" ", " ", " "],
@@ -5,11 +6,14 @@ board = [
     [" ", " ", " "]
 ]
 
+
 def main():
     counter = 0
     while counter < 9:
         if (counter % 2 == 0):
             userinput()
+        else:
+            opponent(counter)
             printboard()
         counter = counter + 1
         
@@ -23,22 +27,28 @@ def printboard():
     print(board[2][0], '|', board[2][1], '|', board[2][2])
 
 def play(row_ref, column_ref):
-
         board[row_ref - 1][column_ref - 1] = "x"
 
 def userinput():
     row_ref = int(input("what row would you like to place your piece? "))
     column_ref = int(input("what column would you like to place your piece? "))
 
-    if row_ref not in range(3) and column_ref not in range(3):
+    while row_ref not in range(3) and column_ref not in range(3) or board[row_ref - 1][column_ref -1] != " ":
         row_ref = int(input("what row would you like to place your piece? "))
-        column_ref = int(input("what column would you like to place your piece? ")) 
-    
-    if(board[row_ref - 1][column_ref - 1] != " "):
-        print("invalid only select an empty space")
-        row_ref = int(input("what row would you like to place your piece? "))
-        column_ref = int(input("what column would you like to place your piece? ")) 
-
+        column_ref = int(input("what column would you like to place your piece? "))
+ 
     play(row_ref, column_ref)
+
+def opponent(counter):
+    count = 0
+    reference = random.randint(1, 9 - counter)
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == " ":
+                count = count + 1
+            if count >= reference:
+                board[i][j] = "o"
+                return
+    
 
 main()
